cff93c7e272f391b2e7e75a8f5e81f3cfa41b1fc
hiskio-daniel <daniel.yeh@hiskio.com>
2021-06-11 23:23:25 +0800

    appwrapper for redux-state , in order to differentiate login state

--- frontend/src/redux/slices/globalSlice.js
+++ frontend/src/redux/slices/globalSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from '@reduxjs/toolkit';
 export const globalSlice = createSlice({
   name: 'global',
   initialState: {
-    isAdminLoggedIn: false,
+    isAdminLoggedIn: true,
     adminInfo: {},
     isLoading: false
   },
